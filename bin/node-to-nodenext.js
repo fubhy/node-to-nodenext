8e475a1ad8811ca60805128525a4239f4827afe0
Sebastian Lorenz <fubhy@fubhy.com>
2023-04-03 13:56:54 +0200

    feat: explicitly support dir (glob) or single file

--- bin/node-to-nodenext.js
+++ bin/node-to-nodenext.js
@@ -3,13 +3,45 @@
 "use strict";
 
 const { run } = require("jscodeshift/src/Runner");
+const glob = require("glob");
+const path = require("path");
+const fs = require("fs");
+const transform = require.resolve("../dist/node-to-nodenext.js");
+
+const args = process.argv.slice(2);
+if (args.length !== 1) {
+  console.error("Usage: `node-to-nodenext <path>`");
+  process.exit(1);
+}
 
 (async () => {
-  const transform = require.resolve("../dist/node-to-nodenext.js");
-  const paths = process.argv.slice(2);
+  const [target] = args;
+  const resolved = path.resolve(target);
 
-  await run(transform, paths, {
+  const stat = fs.lstatSync(resolved);
+  if (stat.isFile()) {
+    await transformFile(resolved);
+  } else if (stat.isDirectory()) {
+    await transformDirectory(resolved);
+  }
+})();
+
+function transformFile(file) {
+  return run(transform, [file], {
     extensions: ["ts", "tsx"].join(","),
     parser: "tsx",
   });
-})();
+}
+
+function transformDirectory(dir) {
+  const paths = glob.sync(["**/*.ts", "**/*.tsx"], {
+    cwd: dir,
+    ignore: ["**/node_modules/**"],
+    absolute: true,
+  });
+
+  return run(transform, paths, {
+    extensions: ["ts", "tsx"].join(","),
+    parser: "tsx",
+  });
+}
